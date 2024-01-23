4a829619d02954459bb828a8ff760fcf581fd509
Jeremiah Samuel <livinalt@gmail.com>
2024-01-23 20:14:40 +0100

    second

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const address= [0x0A68c77b6c71c54cD12366a34f3ee74927f13586,
-                0x7085E7D475E9b3f3f9C3250d5ca46c87ecE1C5c9,
-                0x2398005B4552386059E631594aEca3A61fa12F14,
-                0x2398005B4552386059E631594aEca3A61fa12F14,
-                0x2398005B4552386059E631594aEca3A61fa12F14
+    const address= [
+                    0x0A68c77b6c71c54cD12366a34f3ee74927f13586,
+                    0x7085E7D475E9b3f3f9C3250d5ca46c87ecE1C5c9,
+                    0x2398005B4552386059E631594aEca3A61fa12F14,
+                    0x2398005B4552386059E631594aEca3A61fa12F14,
+                    0x2398005B4552386059E631594aEca3A61fa12F14
                 ]
 
-
+                
+              
